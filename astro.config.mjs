5b38eac074743fe72fc184af0b7198b57d734240
stevey007 <stevejamespage@gmail.com>
2023-10-13 06:53:51 +0800

    2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import robotsTxt from 'astro-robots-txt';
 // https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
+
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  site: 'https://fire-tx.asia',
+  integrations: [tailwind(), react(), robotsTxt()]
 });
